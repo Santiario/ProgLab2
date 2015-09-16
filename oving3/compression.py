e3496f4f2e75d13fc11e65f3026e36383274fa3b
Vemund Santi <vemund@santi.no>
2015-09-16 15:33:21 +0200

    Added Lempel-Ziv-compression, added samplefiles, new version of lempel-ziv PDF, ready for turning in

--- oving3/compression.py
+++ oving3/compression.py
@@ -1,6 +1,7 @@
 import abc
 import PythonLabs.BitLab as btl
 import kdprims as kd
+import math as m
 
 __author__ = 'Vemund'
 
@@ -14,7 +15,7 @@ class Coder:
         message = file.read()
         file.close()
 
-        return message.lower()
+        return message.lower().strip('\n\r')
 
     @abc.abstractmethod
     def encode(self, string):
@@ -63,7 +64,7 @@ class AsciiCoder(Coder):
               str(len(message))+' |', str(len(encoded))+' |', str(len(decoded)))
         if message == decoded:
             print('The message and decoded message match each other.')
-            print('The compression rate is', str(self.compression_rate(len(message), len(encoded)))+'%')
+            print('The compression rate is', str(self.compression_rate(len(message), len(encoded))*100)+'%')
         else:
             print('The message and decoded message do NOT match each other.')
             print('The compression rate is irrelevant. Fix your code first.')
@@ -74,7 +75,7 @@ class HuffmanCoder(Coder):
         self.tree = None
         self.freqs = {}
 
-    def gen_freqs(self, filepath):
+    def gen_freqs(self, filepath='corpus1.txt'):
         self.freqs = kd.calc_char_freqs(filepath)
 
     def build_tree(self, freqs):
@@ -92,11 +93,9 @@ class HuffmanCoder(Coder):
         return btl.huffman_decode(encoded_msg, self.tree)
 
     def compression_rate(self, message_length, encoded_length):
-        return 1 - float(encoded_length)/message_length
+        return 1 - encoded_length/(8.0*message_length)
 
-    def encode_decode_test(self, message, filepath):
-        self.gen_freqs(filepath)
-        self.build_tree(self.freqs)
+    def encode_decode_test(self, message):
         print('The original message was:\n' + message)
         encoded = self.encode(message)
         print('The encoded message is:\n' + encoded.__repr__())
@@ -107,22 +106,122 @@ class HuffmanCoder(Coder):
               str(len(message))+' |', str(len(encoded.__repr__()))+' |', str(len(decoded)))
         if message == decoded:
             print('The message and decoded message match each other.')
-            print('The compression rate is', str(self.compression_rate(len(message), len(encoded.__repr__()))))
+            print('The compression rate is', str(self.compression_rate(len(message), len(encoded.__repr__()))*100)+'%')
         else:
             print('The message and decoded message do NOT match each other.')
             print('The compression rate is irrelevant. Fix your code first.')
 
 
-if __name__ == '__main__':
-    #asciicoder = AsciiCoder()
-    #asciicoder.encode_decode_test('Hello world!')
+class LempelZivCoder(Coder):
 
-    #print('\n\n-------------------\n\n')
+    def encode(self, bit_string):
+        slen = len(bit_string)
+        target = bit_string[0]
+        lookup_table = {'': 0, bit_string[0]: 1}
+        size = 2
+        currloc = 1
+        while currloc < slen:
+            oldseg, newbit = self.find_next_segment(bit_string, currloc, lookup_table)
+            bitlen = m.ceil(m.log(size, 2))
+            index = lookup_table[oldseg]
+            index_bits = self.integer_to_bits(index, bitlen)
+            target += (index_bits + newbit)
+            lookup_table[oldseg + newbit] = size
+            currloc += len(oldseg) + 1
+            size += 1
+        return target
 
+    @staticmethod
+    def find_next_segment(bit_string, loc, table):
+        seg = old_seg = ''
+        newbit = ''
+        while table.get(seg) != None:
+            if loc >= len(bit_string):
+                return seg, ''
+            newbit = bit_string[loc]
+            loc += 1
+            old_seg = seg
+            seg += newbit
+        return old_seg, newbit
+
+    @staticmethod
+    def integer_to_bits(integer, bitlength):
+        bits = str(bin(integer))
+        bits = bits[2:]
+        bits = '0'*(bitlength-len(bits)) + bits  # Legger til 0'er foran teksten om den ikke er riktig lengde
+        return bits
+
+    def decode(self, target):
+        tlen = len(target)
+        source = target[0]
+        lt = ['', target[0]]
+        loc = 1
+        size = 2
+        while loc < tlen:
+            bitlen = m.ceil(m.log(size, 2))
+            index = int(target[loc: loc + bitlen], 2)
+            seg = lt[index]
+            if loc + bitlen < tlen:
+                seg += target[loc + bitlen]
+                size += 1
+                lt.append(seg)
+                loc += 1
+            source += seg
+            loc += bitlen
+        return source
+
+    def encode_decode_test(self, message):
+        print('\nPerforming LZ-compression...')
+        print('The original message was:\n' + message)
+        encoded = self.encode(message)
+        print('The encoded message is:\n' + encoded)
+        decoded = self.decode(encoded)
+        print('The decoded message is:\n' + decoded)
+        print()
+        print('The length of the original, encoded and decoded message, respectively:\n' +
+              str(len(message))+' |', str(len(encoded))+' |', str(len(decoded)))
+        if message == decoded:
+            print('The message and decoded message match each other.')
+            print('The compression rate is', str(self.compression_rate(len(message), len(encoded))*100)+'%')
+        else:
+            print('The message and decoded message do NOT match each other.')
+            print('The compression rate is irrelevant. Fix your code first.')
+
+    def compression_rate(self, message_length, encoded_length):
+        return 1 - float(encoded_length)/message_length
+
+
+def ascii_test(msg='Hello World', filepath=False, lz_flag=False):
+    if filepath:
+        msg = Coder.gen_message_from_file(filepath)
+    AsciiCoder().encode_decode_test(msg)
+    if lz_flag:
+        LempelZivCoder().encode_decode_test(AsciiCoder().encode(msg))
+
+
+def huff_test(msg='Hello World', filepath=False, lz_flag=False):
+    msg = msg.lower()
     huffcoder = HuffmanCoder()
-    huffcoder.encode_decode_test('110110110110110110101101110101010101010101010101010101010101101111000110101101011010101', 'nullen.txt')
-    """
-    Dette blir teit. Huffmankoden tar kun for seg enkeltbokstaver, og det er dermed IKKE MULIG aa faa en kortere
-    kode enn den originale. Huffman må lete etter ORD, ikke enkelttegn.
-    """
+    if filepath:
+        msg = Coder.gen_message_from_file(filepath)
+        huffcoder.gen_freqs(filepath)
+    else:
+        huffcoder.gen_freqs()
+    huffcoder.build_tree(huffcoder.freqs)
+    huffcoder.encode_decode_test(msg)
+    if lz_flag:
+        LempelZivCoder().encode_decode_test(huffcoder.encode(msg).__repr__())
 
+
+def lz_test(msg='00000000000000000000', filepath=False):
+    if filepath:
+        msg = Coder.gen_message_from_file(filepath)
+    LempelZivCoder().encode_decode_test(msg)
+
+
+def main():
+    lz_test(msg='0'*100000)
+
+
+if __name__ == '__main__':
+    main()
