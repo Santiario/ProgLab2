3cdf54b69f012d3ad203bd9fbc39067f8154cd3f
Vemund Santi <vemund@santi.no>
2015-09-28 16:39:35 +0200

    Implemented part 5

--- oving4/neural_network.py
+++ oving4/neural_network.py
@@ -3,6 +3,7 @@ __author__ = 'Vemund'
 from os import listdir
 from os.path import isfile, join
 import operator
+import math
 
 
 class FileHandler:
@@ -42,9 +43,13 @@ class StringHandler:
         neg_ordbok = self.make_dict(neg_filepaths)
         self.remove_stop_words(pos_ordbok, stop_words)
         self.remove_stop_words(neg_ordbok, stop_words)
+        # Pruning
+        self.prune(pos_ordbok, len(pos_filepaths) + len(neg_filepaths), 10)
+        self.prune(neg_ordbok, len(pos_filepaths) + len(neg_filepaths), 10)
 
         for key in pos_ordbok.keys():
             if neg_ordbok.get(key) is None:
+
                 pos_ordbok[key] /= float(pos_ordbok[key])
             else:
                 pos_ordbok[key] /= float(pos_ordbok[key] + neg_ordbok[key])
@@ -67,14 +72,20 @@ class StringHandler:
         for filepath in filepaths:
             word_set = self.make_set_from_string(self.remove_nonalphanumeric(FileHandler.read_file(filepath)))
             for word in word_set:
-                if word == 'onstreet':
-                    print(filepath, 'Fant onstreet!')
+                """if word == 'onstreet':
+                    print(filepath, 'Fant onstreet!')"""
+                    # Used to find typos and other misspelled words during implementing
                 if ordbok.get(word) is None:
                     ordbok[word] = 1
                 else:
                     ordbok[word] += 1
         return ordbok
 
+    def prune(self, dictionary, number_of_texts, percentage):
+        sett = set(dictionary.keys())
+        for key in sett:
+            if dictionary[key] < int(math.floor((percentage * number_of_texts)/100.0)):
+                dictionary.pop(key, None)
 
 class CollectionHandler:
     pass
@@ -86,5 +97,5 @@ if __name__ == '__main__':
     positive_filepaths = f.make_filepath_list('./data/subset/train/pos/')
     negative_filepaths = f.make_filepath_list('./data/subset/train/neg/')
     positive_scores, negative_scores = s.make_word_scores(positive_filepaths,negative_filepaths, stop_words)
-    print(positive_scores)
-    print(negative_scores)
+    print('positive:', positive_scores)
+    print('negative:', negative_scores)
