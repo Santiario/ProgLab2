caa310cfb7dd290f8801ed76811b0b33f1ae84a5
Vemund Santi <vemund@santi.no>
2015-09-28 14:43:03 +0200

    Implemented part 4. Removed all br/ from strings

--- oving4/neural_network.py
+++ oving4/neural_network.py
@@ -7,54 +7,84 @@ import operator
 
 class FileHandler:
 
-    def read_file(self, filepath):
+    @staticmethod
+    def read_file(filepath):
         file = open(filepath, 'r')
         string = ''
         for line in file.readlines():
             string += line.strip() + ' '
         return string.lower()
 
-    def remove_nonalphanumeric(self, string):
+    @staticmethod
+    def make_filepath_list(directory):
+        return [directory + f for f in listdir(directory) if isfile(join(directory, f))]
+
+
+class StringHandler:
+
+    @staticmethod
+    def remove_nonalphanumeric(string):
         result = ''
+        string = string.replace('<br />', '')
         for char in string:
             if char.isalnum() or char == ' ':
                 result += char
         return result
 
-    def make_set(self, string):
+    @staticmethod
+    def make_set_from_string(string):
         return set(string.split())
 
-    def make_filepath_list(self, directory):
-        return [directory + f for f in listdir(directory) if isfile(join(directory, f))]
 
-    def remove_words(self, set1, set2):
-        return set1 - set2
 
-    def make_word_score(self, filepaths, stop_words):
+    def make_word_scores(self, pos_filepaths, neg_filepaths, stop_words):
+        pos_ordbok = self.make_dict(pos_filepaths)
+        neg_ordbok = self.make_dict(neg_filepaths)
+        self.remove_stop_words(pos_ordbok, stop_words)
+        self.remove_stop_words(neg_ordbok, stop_words)
+
+        for key in pos_ordbok.keys():
+            if neg_ordbok.get(key) is None:
+                pos_ordbok[key] /= float(pos_ordbok[key])
+            else:
+                pos_ordbok[key] /= float(pos_ordbok[key] + neg_ordbok[key])
+
+        for key in neg_ordbok.keys():
+            if pos_ordbok.get(key) is None:
+                neg_ordbok[key] /= float(neg_ordbok[key])
+            else:
+                neg_ordbok[key] /= float(pos_ordbok[key] + neg_ordbok[key])
+
+        return sorted(pos_ordbok.items(), key=operator.itemgetter(1), reverse=True), sorted(neg_ordbok.items(), key=operator.itemgetter(1), reverse=True)
+
+    @staticmethod
+    def remove_stop_words(dictionary, stop_words):
+        for word in stop_words:
+            dictionary.pop(word, None)
+
+    def make_dict(self, filepaths):
         ordbok = {}
-
         for filepath in filepaths:
-            word_set = self.remove_words(self.make_set(self.remove_nonalphanumeric(self.read_file(filepath))), stop_words)
+            word_set = self.make_set_from_string(self.remove_nonalphanumeric(FileHandler.read_file(filepath)))
             for word in word_set:
+                if word == 'onstreet':
+                    print(filepath, 'Fant onstreet!')
                 if ordbok.get(word) is None:
                     ordbok[word] = 1
                 else:
                     ordbok[word] += 1
-        for word in ordbok:
-            ordbok[word] /= float(len(filepaths))
-        return sorted(ordbok.items(), key=operator.itemgetter(1), reverse=True)
+        return ordbok
 
 
-class CollectionGenerator:
+class CollectionHandler:
     pass
 
 if __name__ == '__main__':
     f = FileHandler()
-    stop_words = f.make_set(f.remove_nonalphanumeric(f.read_file('./data/stop_words.txt')))
+    s = StringHandler()
+    stop_words = s.make_set_from_string(s.remove_nonalphanumeric(f.read_file('./data/stop_words.txt')))
     positive_filepaths = f.make_filepath_list('./data/subset/train/pos/')
-    positive_ordbok_popularity_tuples = f.make_word_score(positive_filepaths, stop_words)
-    print(positive_ordbok_popularity_tuples)
     negative_filepaths = f.make_filepath_list('./data/subset/train/neg/')
-    negative_ordbok_popularity_tuples = f.make_word_score(negative_filepaths, stop_words)
-    print(negative_ordbok_popularity_tuples)
-
+    positive_scores, negative_scores = s.make_word_scores(positive_filepaths,negative_filepaths, stop_words)
+    print(positive_scores)
+    print(negative_scores)
