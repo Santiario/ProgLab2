969ed9cdb20cf24a3ff707ee7a37afa9b4ec4a75
Vemund Santi <vemund@santi.no>
2015-09-29 14:21:25 +0200

    Implemented part 6. Started working on DataSet

--- oving4/neural_network.py
+++ oving4/neural_network.py
@@ -13,14 +13,6 @@ class FileHandler:
         string = ''
         for line in file.readlines():
             string += line.strip() + ' '
-        """if (filepath[:23] == './data/subset/train/pos'):
-            if  ('hidden' in string):
-                #print('Hidden found in a negative string!')
-                FileHandler.pos_hidden += 1
-        if (filepath[:23] == './data/subset/train/neg'):
-            if  ('hidden' in string):
-                #print('Hidden found in a negative string!')
-                FileHandler.neg_hidden += 1"""
         return string.lower()
 
     @staticmethod
@@ -45,8 +37,8 @@ class Word:
         return self.string
 
     def __repr__(self):
-        return self.string + '. Appeared: ' + str(self.appeared) + '. Popularity:' + str(self.popularity) + \
-               '. InfoValue: ' + str(self.information_value) + '\n'
+        return self.string.rjust(30) + str('  Appeared: ' + str(self.appeared)).ljust(20) + str('  Popularity:' +
+                str(self.popularity)).ljust(30) + str('  InfoValue: ' + str(self.information_value)).ljust(50) + '\n'
 
     def __eq__(self, other):
         if self.string == other.string:
@@ -58,10 +50,11 @@ class Word:
         return self
 
     def __lt__(self, other):
-        return self.information_value < other.information_value
+        return self.appeared < other.appeared
 
     def calculate_popularity(self, number_of_files):
         self.popularity = self.appeared/number_of_files
+
 
 
 class Dictionary:
@@ -87,6 +80,9 @@ class Dictionary:
         else:
             return 0
 
+    def get_word(self, string):
+        return self.words.get(string)
+
     @staticmethod
     def remove_nonalphanumeric(string):
         result = ''
@@ -99,97 +95,77 @@ class Dictionary:
     def remove_word(self, word):
         self.words.pop(word, None)
 
+    def get_words_as_strings(self):
+        strings = []
+        for word in self.words.values():
+            strings.append(word.string)
+        return strings
+
     def remove_words(self, word_list):
         for word in word_list:
             self.remove_word(word)
 
+    def set_info_value(self, word, total_appeared):
+        if self.words.get(word) is None:
+            pass
+        else:
+            self.words[word].information_value = self.words[word].appeared / total_appeared
 
-
-    def make_word_scores(self, pos_filepaths, neg_filepaths, stop_words, percentage, n_gram):
-
-        # Pruning
-        self.prune(pos_ordbok, neg_ordbok, len(pos_filepaths) + len(neg_filepaths), percentage)
-
-
-    def make_words_from_filepaths(self, filepaths):
-        for filepath in filepaths:
-            string_set = set(self.remove_nonalphanumeric(FileHandler.read_file(filepath)).split())
-            for string in string_set:
-                #if string == 'portraywhiteonblackviolenceandwinanoscar':
-                #    print(filepath, string)
-                if string in self.words.keys():
-                    self.words[string].appeared += 1
-                else:
-                    self.words[string] = Word(string)
-
-
-
-    def make_dict(self, filepaths, n_gram=1):
-        ordbok = {}
+    # OPPDATERT
+    def make_words_from_filepaths(self, filepaths, n_grams=1):
         for filepath in filepaths:
             word_list = self.remove_nonalphanumeric(FileHandler.read_file(filepath)).split()
-            word_set = set()
-            for i in range(1, n_gram + 1):
+            for n in range(1, n_grams + 1):
                 index = 0
-                while index < len(word_list) - i:
-                    word = ''
-                    for j in range(i):
-                        word += word_list[index + j] + ' '
-                    word = word[:-1]
-                    if word not in word_set:
-                        pass
-                        # print('La til:', word, 'i ordlista.')
-                    word_set.add(word)
-                    index += 1
-                for word in word_set:
-                    if word == 'pornnow':
-                        print(filepath, 'Fant pornnow!')
-                    # Used to find typos and other misspelled words during implementing
-                    if ordbok.get(word) is None:
-                        ordbok[word] = 1
+                while index <= len(word_list) - n:
+                    word = ' '.join(word_list[index:index + n])
+                    if word in self.words.keys():
+                        self.words[word].appeared += 1
                     else:
-                        ordbok[word] += 1
-        return ordbok
+                        self.words[word] = Word(word)
+                    index += 1
 
-    def prune(self, pos_ordbok, neg_ordbok, number_of_texts, percentage):
-        keys = set(pos_ordbok.keys()).union(neg_ordbok.keys())
-        for key in keys:
-            total = 0
-            if neg_ordbok.get(key) is not None:
-                total += neg_ordbok[key]
-            if pos_ordbok.get(key) is not None:
-                total += pos_ordbok[key]
 
-            if neg_ordbok.get(key) is not None:
-                neg_ordbok[key] /= total
-            if pos_ordbok.get(key) is not None:
-                pos_ordbok[key] /= total
+    def prune(self, divisor, percentage):
+        words = set(self.get_words_as_strings())
+        for word in words:
+            if self.words[word].appeared / float(divisor) < percentage / 100.0:
+                self.remove_word(word)
 
-        for key in neg_ordbok.keys():
-            if pos_ordbok.get(key) is None:
-                neg_ordbok[key] /= float(neg_ordbok[key])
-            else:
-                neg_ordbok[key] /= float(pos_ordbok[key] + neg_ordbok[key])
-        for key in keys:
-            if key == 'hidden':
-                print(number_of_texts)
-            if dictionary[key]/number_of_texts <= percentage/100.0:
-                dictionary.pop(key, None)
-            """else:
-                print(dictionary[key]/number_of_texts)"""
 
+class DataSet:
+
+    def __init__(self, positive_filepaths, negative_filepaths):
+        self.positive_words = Dictionary()
+        self.negative_words = Dictionary()
+        self.positive_filepaths = positive_filepaths
+        self.negative_filepaths = negative_filepaths
+
+    def remove_words(self, word_list):
+        self.negative_words.remove_words(word_list)
+        self.positive_words.remove_words(word_list)
+
+    def make_words_from_filepaths(self, n_grams=1):
+        self.positive_words.make_words_from_filepaths(self.positive_filepaths, n_grams)
+        self.negative_words.make_words_from_filepaths(self.negative_filepaths, n_grams)
 
 if __name__ == '__main__':
     f = FileHandler()
+
     positive_words = Dictionary()
     negative_words = Dictionary()
+
     stop_words = f.make_list_from_file('./data/stop_words.txt')
+
 
     positive_filepaths = f.make_filepath_list('./data/subset/train/pos/')
     negative_filepaths = f.make_filepath_list('./data/subset/train/neg/')
 
-    positive_words.make_words_from_filepaths(positive_filepaths)
-    negative_words.make_words_from_filepaths(negative_filepaths)
+    data = DataSet(positive_filepaths, negative_filepaths)
+
+
+    positive_words.make_words_from_filepaths(positive_filepaths, 2)
+    negative_words.make_words_from_filepaths(negative_filepaths, 2)
 
     for word in positive_words.values():
         word.calculate_popularity(len(positive_filepaths))
@@ -199,20 +175,20 @@ if __name__ == '__main__':
     positive_words.remove_words(stop_words)
     negative_words.remove_words(stop_words)
 
-    for word in positive_words.values():
-        total_appeared = positive_words.get_appeared(word.string) + negative_words.get_appeared(word.string)
-        word.information_value = word.appeared / total_appeared
-    for word in negative_words.values():
-        total_appeared = positive_words.get_appeared(word.string) + negative_words.get_appeared(word.string)
-        word.information_value = word.appeared / total_appeared
+    word_set = set()
+    word_set.update(positive_words.get_words_as_strings())
+    word_set.update(negative_words.get_words_as_strings())
+    for word in word_set:
+        total_appeared = positive_words.get_appeared(word) + negative_words.get_appeared(word)
+        positive_words.set_info_value(word, total_appeared)
+        negative_words.set_info_value(word, total_appeared)
 
-    print(sorted(positive_words.items(), key=operator.itemgetter(1)))
-    print(sorted(negative_words.items(), key=operator.itemgetter(1)))
+    total_files = len(negative_filepaths) + len(positive_filepaths)
+    positive_words.prune(total_files, 1)
+    negative_words.prune(total_files, 1)
+
+    print(sorted(positive_words.values()))
+    print('-----------------------------------------------------------------------------------------------------------------')
+    print(sorted(negative_words.values()))
 
 
-    #positive_scores, negative_scores = s.make_word_scores(positive_filepaths, negative_filepaths, stop_words, 1, n_gram=2)
-    #print(FileHandler.num /(len(positive_filepaths) + len(negative_filepaths)))
-    #print(len(negative_filepaths))
-    #print(len(negative_filepaths))
-    #print('positive:', positive_scores)
-    #print('negative:', negative_scores)
